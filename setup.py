ee4c2222537ac2a6537281b8bf55156a893951fa
saurabhpratap <saurabhp@leewayhertz.com>
2022-04-26 19:17:19 +0530

    code

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name = 'spbasiccalculator',
+    version = '0.0.1',
+    description = 'a very basic addition function',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='https://github.com/saurabhpl/demo',
+    author='Saurabh Pratap Singh',
+    license='MIT',
+    keywords='',
+    packages=find_packages(),
+)
